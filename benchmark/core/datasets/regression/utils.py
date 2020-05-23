9efebfe1f82df133beddf274106716c832f73565
Justin Beland <justin.beland@mail.utoronto.ca>
2020-05-23 18:15:07 -0400

    crude fix for loading data

--- benchmark/core/datasets/regression/utils.py
+++ benchmark/core/datasets/regression/utils.py
@@ -1,14 +1,18 @@
 from scipy.io import loadmat
 import numpy as np
+from benchmark.core.datasets.regression import utils
+
 
 
 def load_data(dataset_name):
 
+    here = utils.__file__.replace('core/datasets/regression/utils.py','')
+
     # construct the filename
-    filename = '../../../datasets/regression/' + dataset_name + '/' + dataset_name + '.mat'
+    filename = str(here + 'datasets/regression/' + dataset_name + '/' + dataset_name + '.mat')
 
     # load the data anc convert to numpy array
-    data = loadmat('../../../datasets/regression/breastcancer/breastcancer.mat')
+    data = loadmat(filename)
     data = np.array(data['data'])
 
     # extract the number of samples and dimensionality of the input
@@ -21,9 +25,12 @@ def load_data(dataset_name):
 
     return n, D, X, Y
 
+'''
 dataset_name = 'breastcancer'
 n, D, X, Y = load_data('breastcancer')
 print(n, D)
+'''
+
 
 
 
